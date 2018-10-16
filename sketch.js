c3a8e10c09561c2fda2e7b0100a6efa2d5a5e6af
BeatriceCurti <43402612+BeatriceCurti@users.noreply.github.com>
2018-10-16 22:01:31 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,60 @@
-function preload(){
-  // put preload code here
+var entities = [];
+var g = 398/60;
+var colorList = ['#86DEB7',
+                 '#63B995',
+                 '#FF6F59',
+                 '#DB504A',
+                 '#FED18C'];
+
+var Entity = function (x, y) {
+    this.color = [random(colorList)];
+    this.x = x;
+    this.y = y;
+    this.speed = [random(-25,25),0];
+    this.size = 70;
+    this.update = function () {
+        this.x += this.speed[0];
+        this.y += this.speed[1];
+        if (this.x + (this.size/2) > width || this.x < 0) {
+            this.speed[0] = -this.speed[0];
+            this.x += (this.speed[0] * 2);
+        }
+        if (this.y + (this.size/2) > height) {
+            this.speed[1] = -this.speed[1]*0.7;
+            this.y += (this.speed[1] * 2);
+        }
+        this.speed[1] += g;
+
+    }
+
+    this.draw = function () {
+        ellipse(this.x, this.y, this.size);
+
+    }
 }
 
+
 function setup() {
-  // put setup code here
+    createCanvas(windowWidth, windowHeight);
+    background(0)
 }
 
 function draw() {
-  // put drawing code here
+        background('#AFFFEC')
+        noStroke();
+
+    for (let i = 0; i < entities.length; i++) {
+        entities[i].update();
+    }
+    for (let i = 0; i < entities.length; i++) {
+        fill(entities[i].color);
+        entities[i].draw();
+    }
+    textSize(20);
+text('click anywhere', 50, 50);
+
+}
+
+document.onclick = function (e) {
+    entities.push(new Entity(e.clientX,e.clientY));
 }
